46ff9590d48dba8e09fcf0d66c632359e1be1c03
Leelz <adam.leel93@gmail.com>
2017-02-22 09:17:06 +0000

    new updated shows file

---
+++ shows/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :favourite_shows
+  has_many :favourites, through: :favourite_shows, source: :show
+
+end
---
+++ shows/db/seeds.rb
@@ -0,0 +1,25 @@
+Show.delete_all()
+
+Show.create({
+  title: 'Silicon Valley', 
+  series: 4,
+  description: "Show about nerds and their start-up trying to make it",
+  image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgwNTUzNzIxM15BMl5BanBnXkFtZTgwMzQ1NTk2ODE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  programmeID: "b013pqnm"
+  })
+
+Show.create({
+  title: 'The Wire', 
+  series: 4,
+  description: "Show about the mean streets of somewhere",
+  image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNjc1NzYwODEyMV5BMl5BanBnXkFtZTcwNTcxMzU1MQ@@._V1_SY1000_CR0,0,735,1000_AL_.jpg",
+  programmeID: "c014pqnm"
+  })
+
+Show.create({
+  title: 'The Thick of It', 
+  series: 2,
+  description: "Political satire",
+  image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNGE4NWViMzItYTE3Zi00NzZjLTk0MDItNTMzMzA5Y2U4MjFiXkEyXkFqcGdeQXVyNTM4OTY2MDU@._V1_.jpg",
+  programmeID: "h232sdls"
+  })
